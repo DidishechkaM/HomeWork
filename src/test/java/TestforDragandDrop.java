7511e631e7c85ad69c03adfcbb2072e062b7e48c
Diana Maksimowa <dianamaksimowa@yandex.ru>
2023-02-27 14:52:34 +0300

    added checks for dragAndDrop

--- src/test/java/TestforDragandDrop.java
+++ src/test/java/TestforDragandDrop.java
@@ -1,30 +1,28 @@
-import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class TestforDragandDrop {
     @Test
-    void DrugAndDropfanny(){
+    void DragAndDropfanny(){
         open("https://the-internet.herokuapp.com/drag_and_drop");
         $("#column-a").dragAndDropTo($("#column-b"));
-        $("#column-b").shouldHave(Condition.text("A"));
+        $("#column-b").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("A"));
         sleep(5000);
-
-
-
-
-
+        
+}
+// Не работающий тест с actions (я и так его и сяк)
+   @Test
+  void Example(){
+       Configuration.browserSize = "1920x1080";
+       open("https://the-internet.herokuapp.com/drag_and_drop");
+       actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).perform();
+       sleep(6000);
+       // Даже так не получилось actions().moveToElement($("column-a")).clickAndHold().moveByOffset(300, 0).release().perform();
 
     }
-
-
-   // Запрограммируйте Drag&Drop с помощью Selenide.actions()
-    // - Откройте https://the-internet.herokuapp.com/drag_and_drop
-    // - Перенесите прямоугольник А на место В
-    // - Проверьте, что прямоугольники действительно поменялись
-    // - В Selenide есть команда $(element).dragAndDrop($(to-element)), проверьте работает ли тест, если использовать её вместо actions()
-
-
 }
