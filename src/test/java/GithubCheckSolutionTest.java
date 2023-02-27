53940770bafee0ea8f0437bb6b03df6ef17aad8c
Diana Maksimowa <dianamaksimowa@yandex.ru>
2023-02-27 11:42:44 +0300

    added fullworktest

--- src/test/java/GithubCheckSolutionTest.java
+++ src/test/java/GithubCheckSolutionTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -20,10 +21,16 @@ public class GithubCheckSolutionTest {
     @Test
     void CheckSolutionForGit() {
       open("https://github.com/");
-     $(byText("Solutions")).hover();
+      $(byText("Solutions")).hover();
+      $(byText("Enterprise")).click();
+      $("main.font-mktg, h1").shouldHave(text("Build like the best"));
+     // $("main.font-mktg").$("h1").shouldHave(text("Build like the best"));Вариант 2
 
 
-    //$(String.valueOf(withText("Solutions"))).hover();
+
+
+
+
 
 
 
