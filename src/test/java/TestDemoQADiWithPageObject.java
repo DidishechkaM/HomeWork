a6194c9fab17f0cd8eb0590784513c53eb1b6a87
Diana Maksimowa <dianamaksimowa@yandex.ru>
2023-03-08 11:20:41 +0300

    added full test with PageObject and Components

--- src/test/java/TestDemoQADiWithPageObject.java
+++ src/test/java/TestDemoQADiWithPageObject.java
@@ -1,13 +1,6 @@
 
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-
-import java.io.File;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
 
 public class TestDemoQADiWithPageObject extends TestBase {
 
@@ -15,45 +8,31 @@ public class TestDemoQADiWithPageObject extends TestBase {
     @Test
     void fillTest() {
         String userName = "Dian";
-
         registrationPage.openPage()
-                .SetFirsName(userName)
-                .SetLastName("Maksimowa")
-                .Setemail("maksimowa@yandex.ru")
-                .SetGender("Female")
-                .SetNumber("8996266848")
-                .SetBirthDay("13", "March", "1993");
-
-
-        $("#subjectsInput").setValue("Math").pressEnter();
-
-
-        $(By.cssSelector("label[for='hobbies-checkbox-1']")).click();
-
-        File fileToUpload = new File("src/test/resources/pictures/temp.png");
-        $("#uploadPicture").uploadFile(fileToUpload);
-
-        $("#currentAddress").setValue("Address");
-        $("#state").click();
-        $("#stateCity-wrapper").$(byText("Uttar Pradesh")).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText("Agra")).click();
-        $("#submit").click();
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-
-
-        $(".modal-body").shouldHave(text("Dian Maksimowa"));
-        $(".modal-body").shouldHave(text("maksimowa@yandex.ru"));
-        $(".modal-body").shouldHave(text("Female"));
-        $(".modal-body").shouldHave(text("8996266848"));
-        $(".modal-body").shouldHave(text("13 March,1993"));
-        $(".modal-body").shouldHave(text("Math"));
-        $(".modal-body").shouldHave(text("Sports"));
-        $(".modal-body").shouldHave(text("temp.png"));
-        $(".modal-body").shouldHave(text("Address"));
-
-        $("#closeLargeModal").click();
-
-
+                .setFirsName(userName)
+                .setLastName("Maksimowa")
+                .setEmail("maksimowa@yandex.ru")
+                .setGender("Female")
+                .setNumber("8996266848")
+                .setBirthDay("13", "March", "1993")
+                .setSubject("Maths")
+                .setCheckbox()
+                .uploadFile("src/test/resources/pictures/temp.png")
+                .setAddress("Address")
+                .setState()
+                .setCity()
+                .setstateCity()
+                .submitclick();
+        registrationPage.verifiability();
+        registrationPage.verifyValueTable("Student Name", userName + " Maksimowa");
+        registrationPage.verifyValueTable("Student Email", "maksimowa@yandex.ru");
+        registrationPage.verifyValueTable("Gender", "Female");
+        registrationPage.verifyValueTable("Mobile", "8996266848");
+        registrationPage.verifyValueTable("Date of Birth", "13 March,1993");
+        registrationPage.verifyValueTable("Subjects", "Maths");
+        registrationPage.verifyValueTable("Hobbies", "Sports");
+        registrationPage.verifyValueTable("Picture", "temp.png");
+        registrationPage.verifyValueTable("Address", "Address");
+        registrationPage.closeModal();
     }
 }
--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -1,18 +1,18 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
 import pages.components.CalendarComponents;
+import pages.components.RegistrationressultModal;
+
+import java.io.File;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationPage {
+    RegistrationressultModal registrationressultModal = new RegistrationressultModal();
     CalendarComponents calendarComponents = new CalendarComponents();
-    private final SelenideElement lastNameInput = $("#lastName");
-    private final SelenideElement firstNameInput = $("#firstName");
-    private final SelenideElement emailInput = $("#userEmail");
-    private final SelenideElement genterRadio = $("#genterWrapper");
-    private final SelenideElement Number = $("#userNumber");
 
 
     public RegistrationPage openPage() {
@@ -22,36 +22,120 @@ public class RegistrationPage {
         return this;
     }
 
+    private final SelenideElement firstNameInput = $("#firstName");
 
-    public RegistrationPage SetFirsName(String value) {
+    public RegistrationPage setFirsName(String value) {
         firstNameInput.setValue(value);
         return this;
     }
 
-    public RegistrationPage SetLastName(String value) {
+    private final SelenideElement lastNameInput = $("#lastName");
+
+    public RegistrationPage setLastName(String value) {
         lastNameInput.setValue(value);
         return this;
     }
 
-    public RegistrationPage Setemail(String value) {
+    private final SelenideElement emailInput = $("#userEmail");
+
+    public RegistrationPage setEmail(String value) {
         emailInput.setValue(value);
         return this;
     }
 
-    public RegistrationPage SetGender(String value) {
-        genterRadio.$(byText(value)).click();
+    private final SelenideElement genderRadio = $("#genterWrapper");
+
+    public RegistrationPage setGender(String value) {
+        genderRadio.$(byText(value)).click();
         return this;
     }
 
-    public RegistrationPage SetNumber(String value) {
+    private final SelenideElement Number = $("#userNumber");
+
+    public RegistrationPage setNumber(String value) {
         Number.setValue(value);
         return this;
     }
 
-    public RegistrationPage SetBirthDay(String day, String month, String year) {
+    private final SelenideElement SetSubject = $("#subjectsInput");
+
+    public RegistrationPage setSubject(String value) {
+        SetSubject.setValue(value).pressEnter();
+        return this;
+    }
+
+    private final SelenideElement SetCheckbox = $(By.cssSelector("label[for='hobbies-checkbox-1']"));
+
+    public RegistrationPage setCheckbox() {
+        SetCheckbox.click();
+        return this;
+    }
+
+    private final SelenideElement UploadFile = $("#uploadPicture");
+
+    public RegistrationPage uploadFile(String path) {
+        UploadFile.uploadFile(new File(path));
+        return this;
+    }
+
+    private final SelenideElement setAddress = $("#currentAddress");
+
+    public RegistrationPage setAddress(String value) {
+        setAddress.setValue(value);
+        return this;
+    }
+
+    private final SelenideElement setState = $("#state");
+
+    public RegistrationPage setState() {
+        setState.click();
+        return this;
+    }
+
+    private final SelenideElement setCity = $("#stateCity-wrapper");
+
+    public RegistrationPage setCity() {
+        setCity.$(byText("Uttar Pradesh")).click();
+        $("#city").click();
+        return this;
+    }
+
+    private final SelenideElement estateCity = $("#stateCity-wrapper");
+
+    public RegistrationPage setstateCity() {
+        estateCity.$(byText("Agra")).click();
+        return this;
+
+    }
+
+    private final SelenideElement clickSubmit = $("#submit");
+
+    public RegistrationPage submitclick() {
+        clickSubmit.click();
+        return this;
+    }
+
+    public RegistrationPage setBirthDay(String day, String month, String year) {
         $("#dateOfBirthInput").click();
         calendarComponents.setDate(day, month, year);
         return this;
 
+    }
+
+    private final SelenideElement closemodal = $("#closeLargeModal");
+
+    public RegistrationPage closeModal() {
+        closemodal.click();
+        return this;
+    }
+
+    public RegistrationPage verifiability() {
+        registrationressultModal.verifyModalText();
+        return this;
+    }
+
+    public RegistrationPage verifyValueTable(String key, String value) {
+        registrationressultModal.verifyValueTable(key, value);
+        return this;
     }
 }
